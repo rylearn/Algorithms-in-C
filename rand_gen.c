be26511f4497919f7571e0d35ed24692e5004e3f
rylearn <40655878+rylearn@users.noreply.github.com>
2018-08-07 16:59:23 -0700

    Added frees, renamed functions

--- rand_gen.c
+++ rand_gen.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_int(int* a, int n) {
+void print_int_array(int* a, int n) {
 	for (int i = 0; i < n; ++i) {
 		printf("%d ", a[i]);
 	}
@@ -11,7 +11,7 @@ void print_int(int* a, int n) {
 }
 
 // returns bool if number in array
-int isIn(int num, int* arr, int n) {
+int contains(int num, int* arr, int n) {
 	for (int i = 0; i < n; ++i) {
 		if (arr[i] == num) {
 			return 1;
@@ -20,25 +20,48 @@ int isIn(int num, int* arr, int n) {
 	return 0;
 }
 
-int* generate_random_from_array(int *arr, int n, int m) {
+int* get_random_subset(int *arr, int n, int m) {
 	srand(time(NULL));
-	int *indices = malloc(m * sizeof(int));
-	int *ret_arr = malloc(m * sizeof(int));
 	int index;
 	int indices_len = 0;
+	int *subset_array = malloc(m * sizeof(int));
+	int *indices = malloc(m * sizeof(int));
 	while (indices_len < m) {
 		index = rand() % n;
-		if (!isIn(index, indices, indices_len)) {
-			indices[indices_len] = index;
-			ret_arr[indices_len] = arr[index];
+		if (!contains(index, indices, indices_len)) {
+			subset_array[indices_len] = arr[index];
 			indices_len += 1;
 		}
 	}
-	return ret_arr;
+	free(indices);
+	return subset_array;
 }
 
 int main() {
-	int arr[] = {1, 4, 2, 8, 19};
-	print_int(gen_random(arr, 5, 2), 2);
+	int m;
+	int n;
+	int *arr;
+
+	printf("Enter size of array: ");
+	scanf("%d", &n);
+
+	arr = malloc(sizeof(int) * n);
+
+	printf("Enter array: ");
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &arr[i]);
+	}
+	
+	printf("Enter size of subset: ");
+	scanf("%d", &m);
+
+	int *subset = get_random_subset(arr, n, m);
+	
+	print_int_array(arr, n);
+	free(arr);
+	
+	print_int_array(subset, m);
+	free(subset);
+
 	return 0;
 }
